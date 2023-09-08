d58e8fde6bf44479fe7719a71199ae88d033d082
GET Academy <terje@kolderup.net>
2023-09-08 13:28:35 +0200

    x

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+function translate(text){
+    let translation = '';
+    for(let letter of text){  
+        translation += 'aeiouyæøå'.includes(letter) ? letter : letter + 'o' + letter;
+    }
+    return translation;
+}
+
 function countA(text) {
     if (typeof text !== "string") return 0;
     let count = 0;
-    for (let letter of text) {
+    for (let letter of text.toLowerCase()) {
         if (letter == 'a') count++;
     }
     return count;
 }
+
+
+// Terje
+// Toterorjoje
