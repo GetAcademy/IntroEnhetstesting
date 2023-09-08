429fc1a58a450bc878f8caaca59ca6194bf6bdbc
GET Academy <terje@kolderup.net>
2023-09-08 14:03:47 +0200

    x

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function translate(text){
+function translate(text) {
     let translation = '';
-    for(let letter of text){  
-        translation += 'aeiouyæøå'.includes(letter) ? letter : letter + 'o' + letter;
+    for (let letter of text) {
+        let addText = letter;
+        const isConsonant = 'bcdfghjklmnpqrstvwxz'.includes(letter.toLowerCase())
+        if (isConsonant) addText += 'o' + letter.toLowerCase();
+        translation += addText;
     }
     return translation;
 }
@@ -15,6 +18,28 @@ function countA(text) {
     return count;
 }
 
+function describeNumber(number) {
+    if (typeof (number) != 'number') return 'not a number ' + number;
+    let description = number + ' ';
+    if (Math.abs(number % 2) == 0) {
+        description += 'partall';
+    } else {
+        description += 'oddetall';
+    }
+    if (number >= 0) {
+        description += ' positivt';
+    } else {
+        description += ' negativt';
+    }
+    return description;
+}
+
 
 // Terje
 // Toterorjoje
+
+// controller
+function doubleNumber() {
+    number *= 2;
+    updateView();
+}
